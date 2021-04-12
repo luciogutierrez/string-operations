63c0288f665847feb88441f2620880177af59a35
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-12 12:05:15 -0500

    Finish Chanllenge 2

---
+++ your-code/actual-script.py
@@ -0,0 +1,9 @@
+print(bag_of_words)
+term_freq = []
+for row in new_corpus:
+    word_freq = []
+    for word in bag_of_words:
+        num_ocurrences = row.count(word)
+        word_freq.append(num_ocurrences)
+     term_freq.append(word_freq)
+print(term_freq)
